b569ec6d90b226952e6fd0e3c4e7cecca81bf0d2
serg22051979-glitch <serg22051979@gmail.com>
2026-09-09 12:58:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 const form = document.getElementById("bookingForm");
 const message = document.getElementById("message");
+const selectedService = document.getElementById("selectedService");
+const serviceCards = document.querySelectorAll(".service-card");
 
 form.addEventListener("submit", function(event) {
     event.preventDefault();
 
     const name = document.getElementById("name").value;
+    const date = document.getElementById("date").value;
+    const time = document.getElementById("time").value;
+
+    const service = selectedService.textContent.replace("Вы выбрали: ", "");
 
     message.textContent =
-        "Спасибо, " + name + "! Ваша заявка принята 🚗";
+        "Спасибо, " + name +
+        "! Запись на услугу «" + service +
+        "» принята на " + date +
+        " в " + time + " 🚗";
 
     form.reset();
 });
-const serviceCards = document.querySelectorAll(".service-card");
-const selectedService = document.getElementById("selectedService");
 
 serviceCards.forEach(function(card) {
     card.addEventListener("click", function() {
