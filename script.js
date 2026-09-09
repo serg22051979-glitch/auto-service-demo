226851bbe2f2d5fd38a25ae266a435ef746b308c
serg22051979-glitch <serg22051979@gmail.com>
2026-09-09 12:27:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,12 @@ form.addEventListener("submit", function(event) {
 
     form.reset();
 });
+const serviceCards = document.querySelectorAll(".service-card");
+
+serviceCards.forEach(function(card) {
+    card.addEventListener("click", function() {
+        document.getElementById("contacts").scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
